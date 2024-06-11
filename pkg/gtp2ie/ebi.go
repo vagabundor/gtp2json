e0c215a060e64a83fcafdaf55946593bb03b7111
Drifter <sgloom@gmail.com>
2024-06-11 11:03:01 +0500

    EBI type

--- pkg/gtp2ie/ebi.go
+++ pkg/gtp2ie/ebi.go
@@ -2,15 +2,18 @@ package gtp2ie
 
 import "fmt"
 
+// EBI represents an EPS Bearer Identity
+type EBI uint8
+
 // DecodeEBI decodes the EBI from a single-byte slice.
 func DecodeEBI(data []byte) (interface{}, error) {
 	if len(data) < 1 {
-		return nil, fmt.Errorf("insufficient data for EBI: expected at least 1 byte, got %d", len(data))
+		return 0, fmt.Errorf("insufficient data for EBI: expected at least 1 byte, got %d", len(data))
 	}
 
-	ebi := data[0] & 0x0F
+	ebi := EBI(data[0] & 0x0F)
 	if ebi < 1 || ebi > 15 {
-		return nil, fmt.Errorf("invalid EBI value: %d", ebi)
+		return 0, fmt.Errorf("invalid EBI value: %d", ebi)
 	}
 
 	return ebi, nil
