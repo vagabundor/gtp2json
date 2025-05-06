fb5dd8e07b41cadc8fbc5d93bb73530a65ecbf6d
Alexander Nikonov <sgloom@gmail.com>
2025-05-06 15:20:06 +0500

    Update import

--- pkg/gtp2ie/selectmode.go
+++ pkg/gtp2ie/selectmode.go
@@ -2,7 +2,7 @@ package gtp2ie
 
 import (
 	"fmt"
-	"gtp2json/config"
+	"github.com/vagabundor/gtp2json/config"
 )
 
 // SelectionModeNames maps the numerical codes to detailed descriptions (3GPPP TS 29.274 8.58)
